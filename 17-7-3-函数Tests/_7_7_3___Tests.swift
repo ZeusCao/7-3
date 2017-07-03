a896c861f3ea1e3c95090cd656f3382785326ac0
曹檑 <1090491594@qq.com>
2017-07-03 15:10:29 +0800

    Initial Commit

---
+++ 17-7-3-函数Tests/_7_7_3___Tests.swift
@@ -0,0 +1,36 @@
+//
+//  _7_7_3___Tests.swift
+//  17-7-3-函数Tests
+//
+//  Created by Zeus on 2017/7/3.
+//  Copyright © 2017年 Zeus. All rights reserved.
+//
+
+import XCTest
+@testable import _7_7_3___
+
+class _7_7_3___Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
